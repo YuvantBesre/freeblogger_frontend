fce179efe564c33335ad83261d1614e334848030
Yuvant Besre <yuvisingh121212@gmail.com>
2021-11-24 00:12:46 +0530

    Add footer

---
+++ src/layouts/components/foot.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="foot flex">
+        <div class="flex links">
+            <v-icon v-for="link in links" :key="link.icon" color="white" class="cursor mx-2 my-1" size="35" @click="goTo(link.url)">
+                {{ link.icon }}
+            </v-icon>
+        </div>
+        <div>
+            {{ new Date().getFullYear() }} Free Blogger. All Rights Reserved.
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'foot',
+
+    data() {
+        return {
+            links : [
+                {
+                    icon : 'mdi-linkedin',
+                    url : 'https://www.linkedin.com/in/yuvantbesre/'
+                },
+                {
+                    icon : 'mdi-github',
+                    url : ''
+                }
+            ]
+        }
+    },
+
+    methods : {
+        goTo(URL) {
+            window.open(URL);
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+.foot {
+    background-color: black;
+    padding: 20px 0;
+    flex-direction: column;
+    justify-content: center;
+}
+
+</style>
--- src/layouts/layout.vue
+++ src/layouts/layout.vue
@@ -4,6 +4,7 @@
         <router-view :key="$route.fullPath"/>
         <login-signup-form />
         <create-blog /> 
+        <foot v-if="$route.name !== 'home'" />
     </div>
 </template>
 
@@ -11,13 +12,15 @@
 import navigationBar from "./components/navigation-bar.vue";
 import loginSignupForm from "../views/Master/login-signup-form.vue";
 import createBlog from "../views/Master/create-blog.vue";
+import foot from './components/foot.vue';
 export default {
     name : 'layout',
 
     components : {
         'navigation-bar' : navigationBar,
         'login-signup-form' : loginSignupForm,
-        'create-blog' : createBlog
+        'create-blog' : createBlog,
+        'foot' : foot
     }
 }
 </script>
@@ -25,5 +28,9 @@ export default {
 <style lang="scss">
 .v-messages__message {
     color: red !important;
+}
+
+.layout {
+    height: 100%;
 }
 </style>
